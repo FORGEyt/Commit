3174a7f0021bdf50e8c5a860709784e435c74b76
Oleg <youoleg212@gmail.com>
2021-11-15 16:00:22 +0300

    create skill block

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close'),
+      about = document.querySelector('.about_li');
+
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
-
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
-
+about.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
