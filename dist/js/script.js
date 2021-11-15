d33aec9e9612c10c91ac3549031362391b3bf4c0
Oleg <youoleg212@gmail.com>
2021-11-15 23:53:20 +0300

    create portfolio block and add link on my web site

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,18 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close'),
-      about = document.querySelector('.about_li');
-
+      closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
+
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
-about.addEventListener('click', () => {
-    menu.classList.remove('active');
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
